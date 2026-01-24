ec7c295d2768d5e84ade57bbc9dd8e0660192880
Weston Ruter <weston@ruter.net>
2026-01-24 13:48:29 -0800

    Add issue reference

--- tools/validate-json.js
+++ tools/validate-json.js
@@ -38,7 +38,7 @@ const schemaCache = new Map();
 function createAjv( AjvConstructor ) {
 	const ajv = new AjvConstructor( {
 		allErrors: true,
-		strict: false, // Needed due to Blueprint JSON Schema error: strict mode: use allowUnionTypes to allow union type keyword at "https://playground.wordpress.net/blueprint-schema.json#/definitions/PHPConstants/additionalProperties" (strictTypes).
+		strict: false, // See <https://github.com/WordPress/wordpress-playground/issues/3178>.
 		loadSchema: fetchSchema,
 	} );
 
