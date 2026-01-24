a0fb0ad4d364d1036fdb05a709b59a47bfda3125
Weston Ruter <weston@ruter.net>
2026-01-24 13:25:48 -0800

    Fix directory name for blueprints

--- tools/validate-json.js
+++ tools/validate-json.js
@@ -121,7 +121,7 @@ async function validateFile( filePath ) {
 
 	const maxBlueprintSizeKB = 1000; // See <https://github.com/WordPress/wordpress.org/blob/e76f2913139cd2c7d9fd26895dda58685d16aa81/wordpress.org/public_html/wp-content/plugins/plugin-directory/cli/class-import.php#L809>.
 	if (
-		filePath === '.wordpress-org/blueprint/blueprint.json' &&
+		filePath === '.wordpress-org/blueprints/blueprint.json' &&
 		content.length > maxBlueprintSizeKB * 1024
 	) {
 		console.error(
